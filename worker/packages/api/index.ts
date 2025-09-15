41e1100fab81d1458c5a36a87d49f44592fb7494
A L <achom_lenz@hotmail.com>
2025-09-15 23:40:36 +0200

    Remove unnecessary package duplicates - only include what each service needs

--- worker/packages/api/index.ts
+++
@@ -1,3 +0,0 @@
-export { appRouter, type AppRouter } from './trpc/root'
-export { router, publicProcedure, protectedProcedure } from './trpc/trpc'
-export type { Context } from './trpc/trpc'
--- worker/packages/ui/index.ts
+++
@@ -1,4 +0,0 @@
-export { Button, buttonVariants } from './components/button'
-export { Card, CardHeader, CardFooter, CardTitle, CardDescription, CardContent } from './components/card'
-export { Badge, badgeVariants } from './components/badge'
-export { cn } from './lib/utils'
